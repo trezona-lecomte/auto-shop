498c09cf05569e047c8c14e9f807cf3965a55a39
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2015-11-28 21:05:11 +1300

    Don't show sign in link once user is authenticated

--- spec/features/authenticate_spec.rb
+++ spec/features/authenticate_spec.rb
@@ -11,6 +11,7 @@ feature "Authentication" do
     given_i_am_on_the_home_page
     when_i_authenticate_using_twitter
     then_i_should_be_signed_in
+    and_i_should_not_see_a_sign_in_link
   end
 
   ### Step definitions ###
@@ -33,5 +34,9 @@ feature "Authentication" do
 
   def then_i_should_be_signed_in
     expect(page).to have_selector(:link, text: "Sign out")
+  end
+
+  def and_i_should_not_see_a_sign_in_link
+    expect(page).not_to have_content("Sign in with Twitter")
   end
 end
